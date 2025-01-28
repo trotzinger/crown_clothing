d48f0065849145051e62e9c62952386a79704f3d
Tim Rotzinger <tim.rotzinger@coldboretechnology.com>
2025-01-28 05:56:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* { <img />} */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+        {/* { <img />} */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+        {/* { <img />} */}
+        <div className="category-body-container">
+          <h2>sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+        {/* { <img />} */}
+        <div className="category-body-container">
+          <h2>womens</h2>
+          <p>Shop Now</p>
+        </div>
+        {/* { <img />} */}
+        <div className="category-body-container">
+          <h2>Mends</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
